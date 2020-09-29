6bac48a0188d58617be15702bf89cb5cd755a69d
Ab <Ab@DESKTOP-4F514TS>
2020-09-29 21:46:48 +0500

    added add-income.js and small modifications in add-income.html

---
+++ js/add-income.js
@@ -0,0 +1,28 @@
+//console.log('in the name of ALLAH')
+// document.getElementById("btnSave").addEventListener('click',()=>{
+//     console.log(document.getElementById("txtIncome").value)
+// })
+
+// $(()=>{
+//     console.log('jquery works fine')
+// })
+
+$("#btnSave").on('click',async()=>{
+    console.log($("#txtIncome").val())
+
+    let result=await fetch('http://localhost:5000/Income/add',{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+          body:JSON.stringify({amount:$("#txtIncome").val()})
+    })
+    if(result.ok){
+        let response=await result.json();
+        console.log(response)
+    }else{
+        console.log(result)
+    }
+
+    $("#txtIncome").val('')
+})

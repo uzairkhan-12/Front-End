41d066d73fba83d9cdb30e592e368d48668894d2
Uzair khan <pk.uzikhan@gmail.com>
2020-10-03 18:43:31 +0500

    small modificaion in add-edit-expenditure type.html , and new file add-edit-expenditure.js

---
+++ js/Add-edit-expenditure.js
@@ -0,0 +1,38 @@
+$(()=>{
+
+    $("#btnSave").on('click',async()=>{
+    let ExpenditureTypeName=$("#txtIncome").val();
+
+    // if(!ExpenditureTypeName || typeof(ExpenditureTypeName)!=="String"){
+    //     $("#emailHelp").removeClass('d-none');
+    //     return;
+    // }
+   
+    
+        let result=await fetch('http://localhost:5000/ExpenditureType/add',{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+              body:JSON.stringify({ExpenditureTypeName})
+        })
+        
+     if(result.ok){
+          window.location.href='/Expenditure-types.html';
+        }else{
+    
+            console.log(result)
+        }
+        $("#txtIncome").val('')
+
+        
+        $("#txtIncome").on('keyup',()=>{
+            if(!ExpenditureTypeName || typeof(ExpenditureTypeName)!=="String"){
+                $("#emailHelp").removeClass('d-none');  
+                console.log('working')
+            }else {
+                $("#emailHelp").addClass('d-none');
+            }
+    })
+    })
+    })

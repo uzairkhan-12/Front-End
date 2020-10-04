0eb638c9220c44aa36908ff7b21407a2ce63b915
Ab <Ab@DESKTOP-4F514TS>
2020-10-04 16:20:21 +0500

    populating data in list

--- js/expenditure-types.js
+++ js/expenditure-types.js
@@ -3,12 +3,24 @@ async function loadIncome(){
     
     if(result.ok){
         let expanditureTypes=await result.json();
-        console.log(expanditureTypes);
-        
-        expanditureTypes.map(x => {
-            
+        expanditureTypes.map((x,index) => {
+        AddRow(x,index) 
         });
     }
-    
     }
+function AddRow(row,index){
+console.log({row})
+let newRow=$("#tempRow").clone();
+newRow.removeClass('hideRow');
+newRow.removeAttr("id");
+newRow.find("#sno").removeAttr("id").text((index+1));
+newRow.find("#typeName").removeAttr("id").text(row.expenditureTypeName);
+newRow.find("#createdDate").removeAttr("id").text(row.addedOn);
+
+$("#tableBody").append(newRow);
+}
+
+
     loadIncome();
+
+    

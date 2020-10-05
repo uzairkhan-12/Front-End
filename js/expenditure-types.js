b82ec65ec5cda13020baf0462154a82345711dda
Ab <Ab@DESKTOP-4F514TS>
2020-10-05 19:25:59 +0500

    adding click event on button in list of expanditure type

--- js/expenditure-types.js
+++ js/expenditure-types.js
@@ -9,18 +9,38 @@ async function loadIncome(){
     }
     }
 function AddRow(row,index){
-console.log({row})
+//console.log({row})
 let newRow=$("#tempRow").clone();
 newRow.removeClass('hideRow');
 newRow.removeAttr("id");
 newRow.find("#sno").removeAttr("id").text((index+1));
 newRow.find("#typeName").removeAttr("id").text(row.expenditureTypeName);
-newRow.find("#createdDate").removeAttr("id").text(row.addedOn);
+newRow.find("#createdDate").removeAttr("id").text(new Date(row.addedOn).toLocaleString());
+newRow.find("#actionButton").on('click',()=>{
+    toggleType(row.id)
+})
+
+if(row.isActive){
+    newRow.find("#actionButton").addClass('btn btn-outline-danger')
+    newRow.find("#actionButton").text('Active, Mark Inactive')
+}else{
+    newRow.find("#actionButton").addClass('btn btn-outline-primary')
+    newRow.find("#actionButton").text('InActive, Mark active')
+}
+newRow.find("#actionButton").removeAttr("id");
+newRow.find("#linkEdit").removeAttr("id").attr('href',"/Add-edit-expenditure-types.html?id="+row.id);
 
 $("#tableBody").append(newRow);
 }
 
+loadIncome();
 
-    loadIncome();
+async function toggleType(typeId){
+    console.log('add more code',typeId)// code to be written by Uzair
+    //
+    //fetch api calling with new url
+    // when OK call loadIncome();
+    // remove un-necessary code
+}
 
     
